403dd4c2c7d1fb24d9e0b64a2a019d6dd9b06790
kevinjmcmahonjr <kevinjmcmahonjr@gmail.com>
2016-12-13 07:40:09 -0500

    Code Formatting

--- tucker-user-roles.php
+++ tucker-user-roles.php
@@ -12,25 +12,24 @@ License:GPLv2
 <?php
  
 function add_tucker_toys_employee_user_role() {
- add_role('tucker_employee',
-            'Tucker Employee',
-            array(
-	 			'delete_others_pages' => true,
-	 			'delete_others_posts' => true,
-	 			'delete_pages' => true,
-	 			'delete_posts' => true,
-	 			'delete_private_pages' => true,
-	 			'delete_private_posts' => true,
-	 			'delete_published_pages' => true,
-	 			'delete_published_posts' => true,
-	 			'edit_others_poages' => true,
-	 			'edit_others_posts' => true,
-	 			'edit_pages' => true,
-	 			'edit_posts' => true,
-	 			'edit_private_pages' => true,
-	 			'edit_private_posts' => true,
-                'read' => true,
-                'upload_files' => true,
+	add_role('tucker_employee', 'Tucker Employee',
+			 array(
+				'delete_others_pages' => true,
+				'delete_others_posts' => true,
+				'delete_pages' => true,
+				'delete_posts' => true,
+				'delete_private_pages' => true,
+				'delete_private_posts' => true,
+				'delete_published_pages' => true,
+				'delete_published_posts' => true,
+				'edit_others_poages' => true,
+				'edit_others_posts' => true,
+				'edit_pages' => true,
+				'edit_posts' => true,
+				'edit_private_pages' => true,
+				'edit_private_posts' => true,
+				'read' => true,
+				'upload_files' => true,
             )
         );
    }
