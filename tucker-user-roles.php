52b73cbcb57c30d6f11ba9f8695b29f0919c710c
kevinjmcmahonjr <kevinjmcmahonjr@gmail.com>
2016-12-13 18:10:12 -0500

    Add Multisite Compatibility

--- tucker-user-roles.php
+++ tucker-user-roles.php
@@ -12,8 +12,9 @@ License:GPLv2
 <?php
  
 function add_tucker_toys_employee_user_role() {
-	add_role('tucker_employee', 'Tucker Employee',
-			 array(
+	function add_new_tucker_employee_role(){
+		add_role('tucker_employee', 'Tucker Employee',
+			array(
 				'delete_others_pages' => true,
 				'delete_others_posts' => true,
 				'delete_pages' => true,
@@ -30,8 +31,22 @@ function add_tucker_toys_employee_user_role() {
 				'edit_private_posts' => true,
 				'read' => true,
 				'upload_files' => true,
-            )
-        );
-   }
+			)
+		);
+	}
+	if ( is_multisite() && $network_wide ) { 
+
+        global $wpdb;
+
+        foreach ($wpdb->get_col("SELECT blog_id FROM $wpdb->blogs") as $blog_id) {
+            switch_to_blog($blog_id);
+			add_new_tucker_employee_role();
+            restore_current_blog();
+        } 
+
+    } else {
+        add_new_tucker_employee_role();
+    }
+}
 register_activation_hook( __FILE__, 'add_tucker_toys_employee_user_role' );
 ?>
